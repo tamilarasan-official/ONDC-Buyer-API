46d8b0dde17eecc9916c2c81062854538cf5dc7d
prasanthkumar-valar <prasanthkumar@valardigital.in>
2025-08-28 14:37:07 +0530

    feat(dish): added CRUD end points

--- src/dish/dish.controller.ts
+++ src/dish/dish.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { DishService } from './dish.service';
 import { CreateDishDto } from './dto/create-dish.dto';
 import { UpdateDishDto } from './dto/update-dish.dto';
+import { PaginationDto } from 'src/shared/dto/pagination.dto';
 
 @Controller('dish')
 export class DishController {
   constructor(private readonly dishService: DishService) {}
 
   @Post()
-  create(@Body() createDishDto: CreateDishDto) {
+  async create(@Body() createDishDto: CreateDishDto) {
     return this.dishService.create(createDishDto);
   }
 
   @Get()
-  findAll() {
-    return this.dishService.findAll();
+  async findAll(@Query() paginationDto: PaginationDto) {
+    return this.dishService.findAll(paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.dishService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDishDto: UpdateDishDto) {
+  async update(@Param('id') id: string, @Body() updateDishDto: UpdateDishDto) {
     return this.dishService.update(+id, updateDishDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     return this.dishService.remove(+id);
   }
 }
--- src/dish/dto/create-dish.dto.ts
+++ src/dish/dto/create-dish.dto.ts
@@ -1,1 +1,20 @@
-export class CreateDishDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateDishDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    icon?: string;
+
+    @IsBoolean()
+    @IsOptional()
+    status?: boolean;
+
+}
