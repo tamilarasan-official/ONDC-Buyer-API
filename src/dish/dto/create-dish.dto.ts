250c49e5567ae7d8dbc533c6744be227327bf071
sathish <sathish@valardigital.in>
2025-11-01 19:01:45 +0530

    feat: add food_type validation to CreateDishDto
    - Introduced a new validation rule for the food_type field in CreateDishDto, allowing only specific values: 'pure-veg', 'veg', 'non-veg', 'egg', and 'veg-and-non-veg'.
    - This enhancement ensures that the food type input adheres to predefined categories, improving data integrity and user input validation.
    
    These changes enhance the robustness of the dish creation process by enforcing stricter validation rules.

--- src/dish/dto/create-dish.dto.ts
+++ src/dish/dto/create-dish.dto.ts
@@ -1,8 +1,10 @@
-import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsIn, IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { Transform } from "class-transformer";
 import { ApiProperty } from "@nestjs/swagger";
 
+
 export class CreateDishDto {
+  
   @ApiProperty({
     description: "Name of the dish",
     example: "Pizza",
@@ -28,6 +30,7 @@ export class CreateDishDto {
   })
   @IsString()
   @IsNotEmpty()
+  @IsIn(['pure-veg','veg', 'non-veg', 'egg', 'veg-and-non-veg'])
   food_type: string;
 
   @ApiProperty({
